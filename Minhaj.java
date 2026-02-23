354a1bc3e57ab8d5386736768c50bb45a5b9a8a1
Minhajul43 <minhajul255685islam@gmail.com>
2026-02-23 22:19:27 +0600

    Minhajul

---
+++ Minhaj.java
@@ -0,0 +1,8 @@
+public class Minhaj {
+  public static void main(String[] args) {
+    System.out.println("This is an input file.");
+    System.err.println("I have a plan");
+    System.err.println("To make sure that I am not a failure");
+    System.err.println("Because I am a success");
+  }
+}
